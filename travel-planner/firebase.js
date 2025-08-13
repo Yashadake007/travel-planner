13b452b1c0aa73a370af4af5a1381b231918ad00
Yashadake007 <adakeyash447@gmail.com>
2025-08-13 14:45:26 +0530

    Update firebase.js

--- travel-planner/firebase.js
+++ travel-planner/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBU7SRrijoXJzNJdOhQLmSh_9mdoklxLPA",
   authDomain: "travel-planner-bbd7b.firebaseapp.com",
   projectId: "travel-planner-bbd7b",
-  storageBucket: "travel-planner-bbd7b.appspot.com", // important: appspot.com
+  storageBucket: "travel-planner-bbd7b.firebasestorage.app",
   messagingSenderId: "901740451346",
   appId: "1:901740451346:web:b0cb6372d6a7432e932acb"
 };
@@ -17,3 +17,4 @@ const db = firebase.firestore();
 // Optional: expose for console debugging
 window.auth = auth;
 window.db = db;
+
