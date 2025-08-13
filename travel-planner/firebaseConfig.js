5d693dd2e13bc5ce269462b4984af6c0886b0a3c
Yashadake007 <adakeyash447@gmail.com>
2025-08-13 15:12:09 +0530

    Create firebaseConfig.js

---
+++ travel-planner/firebaseConfig.js
@@ -0,0 +1,17 @@
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBU7SRrijoXJzNJdOhQLmSh_9mdoklxLPA",
+  authDomain: "travel-planner-bbd7b.firebaseapp.com",
+  projectId: "travel-planner-bbd7b",
+  storageBucket: "travel-planner-bbd7b.appspot.com",
+  messagingSenderId: "901740451346",
+  appId: "1:901740451346:web:b0cb6372d6a7432e932acb"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
