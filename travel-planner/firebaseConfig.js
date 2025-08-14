a53a8a8bfea0ac70b502ba92c42272361efa63a8
Yashadake007 <adakeyash447@gmail.com>
2025-08-14 12:48:45 +0530

    Delete travel-planner/firebaseConfig.js

--- travel-planner/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-// firebaseConfig.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyBU7SRrijoXJzNJdOhQLmSh_9mdoklxLPA",
-  authDomain: "travel-planner-bbd7b.firebaseapp.com",
-  projectId: "travel-planner-bbd7b",
-  storageBucket: "travel-planner-bbd7b.appspot.com",
-  messagingSenderId: "901740451346",
-  appId: "1:901740451346:web:b0cb6372d6a7432e932acb"
-};
