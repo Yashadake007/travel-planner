4b7bb84adeff53ee54025dcae449212ff2056642
Yashadake007 <adakeyash447@gmail.com>
2025-08-14 10:53:44 +0530

    Update firebaseConfig.js

--- travel-planner/firebaseConfig.js
+++ travel-planner/firebaseConfig.js
@@ -1,6 +1,5 @@
 // firebaseConfig.js
-// Your Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBU7SRrijoXJzNJdOhQLmSh_9mdoklxLPA",
   authDomain: "travel-planner-bbd7b.firebaseapp.com",
   projectId: "travel-planner-bbd7b",
@@ -8,9 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "901740451346",
   appId: "1:901740451346:web:b0cb6372d6a7432e932acb"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Initialize Firebase services
-const auth = firebase.auth();
-const db = firebase.firestore();
